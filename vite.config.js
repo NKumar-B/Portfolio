88285976e3ef92ddf11771246d9bd44dd0baa430
NithinKumar-7893 <nithinkumarbadduluri@gmail.com>
2026-02-15 13:45:51 +0530

    Fixed Preamble

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,28 @@
 
 
 
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 5173,
+//     strictPort: true, // Forces consistent port use
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
     port: 5173,
-    strictPort: true, // Forces consistent port use
+    strictPort: true,
   }
 })
